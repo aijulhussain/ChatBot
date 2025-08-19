1e64b39b9d70d558d812140111ee01f266f83b28
aijulhussain <aijulhussain14336@gmail.com>
2025-08-20 02:06:17 +0530

    setup sqlite database

---
+++ backend/langgraph_backend_sqlite.py
@@ -0,0 +1,43 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import sqlite3
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.1-8b-instant")
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+
+conn= sqlite3.connect(database='chatbot.db', check_same_thread=False)  # Ensure the database is created
+
+# Checkpointer
+checkpointer = SqliteSaver(conn=conn)
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+CONFIG= {'configurable': {'thread_id': 'thread-1'}}
+
+response = chatbot.invoke(
+                {'messages': [HumanMessage(content='Hello, what is my name?')]},
+                config= CONFIG,
+            )
+print(response)
