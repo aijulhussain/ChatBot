cd9de64e3b136b22b484bef5cf92e76f58898730
aijulhussain <aijulhussain14336@gmail.com>
2025-08-20 02:24:48 +0530

    streamlit app add databse

--- backend/langgraph_backend_sqlite.py
+++ backend/langgraph_backend_sqlite.py
@@ -42,9 +42,8 @@ chatbot = graph.compile(checkpointer=checkpointer)
 #             )
 # print(response)
 
-def retrieve_all_treads():
+def retrieve_all_threads():
     all_threads=set()
     for checkpoint in checkpointer.list(None):
         all_threads.add(checkpoint.config['configurable']['thread_id'])
-
         return list(all_threads)
